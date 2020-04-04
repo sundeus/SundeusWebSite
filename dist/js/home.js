de0aadcce51b52d5b467b873f4c74393d3000da4
SwapnaJadhav <swapnajadhav396@gmail.com>
2020-04-04 22:11:16 +0530

    validation
    validation

--- dist/js/home.js
+++ dist/js/home.js
@@ -26,91 +26,138 @@ function myFunction() {
 
 
   function validation(){
-    var name = document.getElementById("name").value;
-    var lname = document.getElementById("lname").value;
+    var name = document.getElementById("name").value.trim();
+    var lname = document.getElementById("lname").value.trim();
 
-    var email = document.getElementById("email").value;
-    var company = document.getElementById("company").value;
-    var mobile = document.getElementById("mobile").value;
-     var selectname = document.getElementById("exampleFormControlSelect1").value;
-    var comment = document.getElementById("comment").value;
+    var email = document.getElementById("email").value.trim();
+    var company = document.getElementById("company").value.trim();
+    var mobile = document.getElementById("mobile").value.trim();
+     var selectname = document.getElementById("exampleFormControlSelect1").value.trim();
+    var comment = document.getElementById("comment").value.trim();
+// console.log(name.length);
+// console.log(lname);
+// console.log(email);
 
+// console.log(company);
+// console.log(mobile);
+// console.log(selectname);
+// console.log(comment.length);
+
+var flag = true;
 
     if(name == ""){
       document.getElementById('username').innerHTML = "Please Enter FirstName";
-      return false;
+      // return false;
+      flag = false;
     }
-    if((name.length <2 ) || (name.length > 20)){
+    else if((name.length <2 ) || (name.length > 20)){
       document.getElementById('username').innerHTML = "Please Enter name length in between 2 and 20";
-      return false;
+      // return false;
+      flag = false;
     }
-    if(!isNaN(name)){
+    else if(!isNaN(name)){
       document.getElementById('username').innerHTML = "Only characters are allowed";
-      return false;
+      // return false;
+      flag = false;
+    }
+    else{
+      document.getElementById('username').innerHTML="";
     }
     // LAST NAME
     if(lname == ""){
       document.getElementById('lastname').innerHTML = "Please Enter LastName";
-      return false;
+      // return false;
+      flag = false;
     }
-    if((lname.length <=2 ) || (lname.length > 20)){
+    else if((lname.length <=2 ) || (lname.length > 20)){
       document.getElementById('lastname').innerHTML = "Please Enter name length in between 2 and 20";
-      return false;
+      // return false;
+      flag = false;
     }
-    if(!isNaN(lname)){
+    else if(!isNaN(lname)){
       document.getElementById('lastname').innerHTML = "Only characters are allowed";
-      return false;
+      // return false;
+      flag = false;
     }
+    else
+{      document.getElementById('lastname').innerHTML = "";
+}
     // EMAIL VALIDATION
     if(email == ""){
       document.getElementById('emails').innerHTML = "Please Enter valid EmailId";
-      return false;
+      // return false;
+      flag=false;
+
     }
-    if(email.indexOf('@') <= 0){
+    else if(email.indexOf('@') <= 0){
       document.getElementById('emails').innerHTML = "@ invalid position";
-      return false;
+      // return false;
+      flag = false;
     }
-    if((email.charAt(email.length-4)!= '.') && (email.charAt(email.length-3)!= '.')){
+   else  if((email.charAt(email.length-4)!= '.') && (email.charAt(email.length-3)!= '.')){
       document.getElementById('emails').innerHTML = ". invalid position";
-      return false;
+      // return false;
+      flag=false;
+    }
+    else {
+      document.getElementById('emails').innerHTML = "";
+
     }
     // COMPANY
     if(company == ""){
       document.getElementById('companyname').innerHTML = "Please Enter valid CompanyName";
-      return false;
+      // return false;
+      flag =false;
     }
-    if((company.length <=2 ) || (company.length > 15)){
+    else if((company.length <=2 ) || (company.length > 15)){
       document.getElementById('companyname').innerHTML = "Please Enter name length in between 2 and 15";
-      return false;
+      // return false;
+
+      flag=false;
+    }
+    else {
+      document.getElementById('companyname').innerHTML = "";
+
     }
     // PHONE VALIDATIONS
     if(mobile == ""){
       document.getElementById('phonenum').innerHTML = "Please Enter Correct Mobile No";
-      return false;
+      flag =false;
     }
-    if(isNaN(mobile)){
+    else if(isNaN(mobile)){
       document.getElementById('phonenum').innerHTML = "User must write digits only";
-      return false;
+      flag= false;
     }
-    if(mobile.length!=10){
+    else if(mobile.length!=10){
       document.getElementById('phonenum').innerHTML = "Mobile number must be 10 digits only";
-      return false;
+      flag= false;
+    }
+    else{
+      document.getElementById('phonenum').innerHTML = "";
+
     }
     // COMPANY FIELD
     if(selectname == "Choose Enquiry"){
       document.getElementById('chooseopt').innerHTML = "Please Select Enquiry Name";
-      return false;
+      flag= false;
+    }
+    else {
+      document.getElementById('chooseopt').innerHTML = "";
+
     }
     // TEXTAREA MESSAGE
     if(comment == ""){
       document.getElementById('message').innerHTML = "Please Provide valid Message";
-      return false;
+      flag = false;
     }
-    if((comment.length <=2 ) || (comment.length > 50)){
+  
+   else if((comment.length <=2 ) || (comment.length > 50)){
       document.getElementById('message').innerHTML = "Please enter Meassage length in between 2 and 50";
-      return false;
+      flag= false;
     }
-    return true;
+    else {      document.getElementById('message').innerHTML = "";
+  }
+    return flag;
   }
 //function myFunction()
 //{
