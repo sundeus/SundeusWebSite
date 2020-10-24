e3c91adc6ced3a23976294ebc2713b1b8d90b95e
Swapna Jadhav <59640817+SwapnaJadhav@users.noreply.github.com>
2020-10-24 08:51:44 +0530

    onboarding link
    done

--- dist/js/home.js
+++ dist/js/home.js
@@ -1,13 +1,3 @@
-// $(document).ready(function () {
-//     $(function () {
-//         $('li a').click(function (e) {
-//             e.preventDefault();
-//             $('a').removeClass('active');
-//             $(this).addClass('active');
-//         });
-//     });
-// });
-
 function myFunction() {
     var popup = document.getElementById("myPopup");
    var validateStatus= validation();
@@ -167,35 +157,4 @@ var flag = true;
   }
     return flag;
   }
-//function myFunction()
-//{
-   /* var h= document.createElement("div");
-      h.className="Popup";
-      var span=document.createElement("span");
-      span.className="popuptext";
-      span.innerText="Sample popup";
-      h.append(span);
-      $("#popup_id").append(h);*/
 
-  //var html= " <div class="Popup" >Send<span class="popuptext" id="myPopup">A Simple Popup!</span></div>";
-
-//}
-
-// });
-
-// function myFunction() {
-//   var popup = document.getElementById("myPopup");
-//   popup.classList.toggle("show");
-// }
-// $(function () {
-//     $("#dialog").dialog({
-//         modal: true,
-//         autoOpen: false,
-//         title: "jQuery Dialog",
-//         width: 300,
-//         height: 150
-//     });
-//     $("#btn btn-primary").click(function () {
-//         $('#dialog').dialog('open');
-//     });
-// });
